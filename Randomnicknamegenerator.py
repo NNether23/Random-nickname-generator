1d31ee4618d490cb2ae4fc8dacad15a64d8cbff4
NNether23 <97787002+NNether23@users.noreply.github.com>
2022-07-11 13:33:48 +0200

    Update Randomnicknamegenerator.py

---
+++ Randomnicknamegenerator.py
@@ -0,0 +1,73 @@
+import random
+
+random1 = random.randint(1, 10)
+random2 = random.randint(1, 10)
+random3 = random.randint(1, 100)
+
+
+#random 1 
+if random1 == 1:
+	nick1 = 'super'
+
+if random1 == 2:
+	nick1 = 'mega'
+
+if random1 == 3:
+	nick1 = 'speedy'
+
+if random1 == 4:
+	nick1 = 'scary'
+
+if random1 == 5:
+	nick1 = 'cute'
+
+if random1 == 6:
+	nick1 = 'sweet'
+
+if random1 == 7:
+	nick1 = 'spicy'
+
+if random1 == 8:
+	nick1 = 'smart'
+
+if random1 == 9:
+	nick1 = 'dead'
+
+if random1 == 10:
+	nick1 = 'funny'
+
+
+#random 2
+
+if random2 == 1:
+	nick2 = 'panda'
+
+if random2 == 2:
+	nick2 = 'horse'
+
+if random2 == 3:
+	nick2 = 'gamer'
+
+if random2 == 4:
+	nick2 = 'programmer'
+
+if random2 == 5:
+	nick2 = 'genius'
+
+if random2 == 6:
+	nick2 = 'chicken'
+
+if random2 == 7:
+	nick2 = 'guy'
+
+if random2 == 8:
+	nick2 = 'nerd'
+
+if random2 == 9:
+	nick2 = 'zombie'
+
+if random2 == 10:
+	nick2 = 'bear'
+
+#printing
+print('Your new nickname is:', nick1, nick2, random3 )
